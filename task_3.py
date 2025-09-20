f35039e7875dd4e7fad67805fb6f7d45b64109b3
raflavanda70-droid <raflavanda70@gmail.com>
2025-09-20 21:49:31 +0300

    Delete task_3.py

--- task_3.py
+++
@@ -1,7 +0,0 @@
-pas = input("Введите пароль: ")
-if len(pas) < 16:
-    print("Слишком короткий")
-elif pas.isdigit() or pas.isalpha():
-    print("Слабый пароль")
-else:
-    print("Надежный пароль")
