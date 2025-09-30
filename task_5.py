e3ac4cc4f891852e091a5849dc24eeb8dee675d2
raflavanda70-droid <raflavanda70@gmail.com>
2025-09-30 13:49:22 +0300

    Delete task_5.py

--- task_5.py
+++
@@ -1,9 +0,0 @@
-number = input("Введите число: ")
-sum = 0
-for digit in number:
-    sum += int(digit)
-num = int(number)
-if num % 7 == 0:
-    print("Магическое число!")
-else:
-    print("Сумма цифр:", sum)
